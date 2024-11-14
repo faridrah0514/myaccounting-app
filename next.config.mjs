82a8ce6d8ca798772a9cdef67b7791f2ec32b00d
Farid Rahman <farid.rahman@grabtaxi.com>
2024-11-14 23:47:53 +0700

    downgrade nextjs version

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // env: {
+  //     MYSQL_HOST: process.env.MYSQL_HOST,
+  //     MYSQL_USERNAME: process.env.MYSQL_USERNAME,
+  //     MYSQL_PWD: process.env.MYSQL_PWD,
+  //     MYSQL_DATABASE: process.env.MYSQL_DATABASE, 
+  //     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  //     NEXTAUTH_URL: process.env.NEXTAUTH_URL  
+  // }
+};
+
+export default nextConfig;
